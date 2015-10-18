373572c97742dc64406149a914a429e503e4626f
aisensiy <aisensiy@gmail.com>
2015-10-18 16:01:12 +0800

    given 1 return 1 1 1

--- src/main/java/ProdFib.java
+++ src/main/java/ProdFib.java
@@ -1,5 +1,8 @@
 public class ProdFib {
     public static long[] productFib(int n) {
+        if (n == 1) {
+            return new long[]{1, 1, 1};
+        }
         return new long[]{0, 1, 1};
     }
 }
--- src/test/java/ProdFibTest.java
+++ src/test/java/ProdFibTest.java
@@ -7,6 +7,11 @@ public class ProdFibTest {
     @Test
     public void should_return_0_1_1_given_0() throws Exception {
         assertThat(ProdFib.productFib(0), is(new long[]{0, 1, 1}));
+    }
+
+    @Test
+    public void should_return_1_1_1_given_1() throws Exception {
+        assertThat(ProdFib.productFib(1), is(new long[]{1, 1, 1}));
 
     }
 }
